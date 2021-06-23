ad0c672da1ce38e7cb31167986772b334ce55cd4
d4talouh <holdentlol@gmail.com>
2021-06-23 21:52:17 +0300

    registered models and updated gitignore

--- social_network/api/admin.py
+++ social_network/api/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
-from .models import CustomUser
+from .models import CustomUser, Reaction, Post
 
 # Register your models here.
 
 admin.site.register(CustomUser)
+admin.site.register(Reaction)
+admin.site.register(Post)
