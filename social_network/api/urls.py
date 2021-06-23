dc743c1f63e18ea7ca2218a0ccc7d822d8d60eb6
d4talouh <holdentlol@gmail.com>
2021-06-23 21:35:51 +0300

    added user registration url

--- social_network/api/urls.py
+++ social_network/api/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from .views.user import UserRetrieveAPIView
+from .views.user import UserRetrieveAPIView, UserRegistrationApiView
 
 urlpatterns = [
-    path(r'users/', UserRetrieveAPIView.as_view(), name='user-list')
+    path(r'users/', UserRetrieveAPIView.as_view(), name='user-list'),
+    path(r'users/registration/', UserRegistrationApiView.as_view(), name='user-registration')
 ]
